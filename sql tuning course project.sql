5506ad8c825e36a71cee0b4f2d3c633569c4e621
harithagoli <88888888+harithagoli@users.noreply.github.com>
2022-02-10 07:35:32 -0600

    Add files via upload

--- sql tuning course project.sql
+++ sql tuning course project.sql
@@ -31,33 +31,43 @@ checked the cost of the query run time and execution plan
 (3) What method you chose to resolve the bottleneck
 created index*/
 -- 3. List the names of students who have taken course v4 (crsCode).
-explain analyze
 /*SELECT name FROM Student WHERE id IN (SELECT studId FROM Transcript WHERE crsCode = @v4);*/
-with cte as (SELECT studId FROM Transcript WHERE crsCode = @v4)
-sELECT name FROM Student join cte on id = studid;
+create index crscode_index on Transcript(crscode);
+explain analyze
+sELECT name FROM Student where id in(SELECT studId FROM Transcript WHERE crsCode = @v4);
+
 /*(1)What was the bottleneck?
-used sub query instead of CTE
+no index
 (2)How did you identify it?
 checked the cost of the query run time and execution plan
 (3) What method you chose to resolve the bottleneck
-wrote cte and called in final query*/
+created index on crscode */
 -- 4. List the names of students who have taken a course taught by professor v5 (name).
+/* SELECT name FROM Student,
+	(SELECT studId FROM Transcript,
+		(SELECT crsCode, semester FROM Professor
+			JOIN Teaching
+			WHERE Professor.name = @v5 AND Professor.id = Teaching.profId) as alias1
+	WHERE Transcript.crsCode = alias1.crsCode AND Transcript.semester = alias1.semester) as alias2
+WHERE Student.id = alias2.studId;*/
 Explain Analyze
-with professor as 
+/*with professor as 
 			(SELECT crsCode,name FROM Professor
 			JOIN course on Professor.deptid = course.deptid
             WHERE Professor.name = @v5 
-            ) 
- select  distinct std.name  from professor pro  join  transcript tra
+            ) */
+ select  distinct std.name  from (SELECT distinct crsCode,name FROM Professor
+			JOIN course on Professor.deptid = course.deptid
+            WHERE Professor.name = @v5 ) pro  join  transcript tra
  on pro.crsCode =  tra.crsCode
 	join student std on tra.studid = std.id
-    order by tra.crscode;
+    order by std.name;
    /*(1)What was the bottleneck?
 query took long time and 
 (2)How did you identify it?
 checked the cost of the query run time and execution plan
 (3) What method you chose to resolve the bottleneck
-created CTE*/ 
+created subquery*/ 
     
 -- 5. List the names of students who have taken a course from department v6 (deptId), but not v7.
 /*explain  analyze
@@ -108,15 +118,23 @@ SET @v8 = 'MAT';
 WHERE id = alias.studId;*/
 
 explain analyze
-select  distinct name  from
-student  s join transcript  t on s.id = t.studid
-where   crscode  in (SELECT distinct crscode FROM springboardopt.course where deptid =  @v8)  
+WITH v8_courses AS (
+	SELECT crsCode 
+    FROM Course 
+    WHERE deptId = @v8
+)
+SELECT s.name
+FROM Student AS s, Transcript AS t, v8_courses AS v8c
+WHERE t.studId = s.id
+AND v8c.crsCode = t.crsCode
+GROUP BY s.name
+HAVING COUNT(*) = (SELECT COUNT(*) FROM v8_courses)
   /*(1)What was the bottleneck?
 query took long time and too many subquries used
 (2)How did you identify it?
 checked the cost of the query run time and execution plan
 (3) What method you chose to resolve the bottleneck
-used only  one subquery*/
+used CTE*/
 
 
 
